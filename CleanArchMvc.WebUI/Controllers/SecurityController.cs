503e7aa42f3d2a63800c61d41cd33dcc45f5cc5a
Murillo Nogueira <murillo.nogueira@qdois.com.br>
2022-05-25 20:15:54 -0300

    Implementando front-end e controllers

---
+++ CleanArchMvc.WebUI/Controllers/SecurityController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    [Authorize]
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
